73ab81e006952a1aef3c66f110e1226a83b5acbc
Stijn Leenknegt <stijn@diagro.be>
2022-05-06 09:59:33 +0200

    version 1.5

---
+++ src/CompanyUserBroadcast.php
@@ -0,0 +1,19 @@
+<?php
+namespace Diagro\Events;
+
+abstract class CompanyUserBroadcast
+{
+
+    use BroadcastWhenOccupied;
+
+
+    public function __construct(?int $company_id = null, ?int $user_id = null)
+    {
+        if($company_id == null) $company_id = auth()->user()->company()->id();
+        if($user_id == null) $user_id = auth()->user()->id();
+
+        $this->user_id = $company_id . '.' . $user_id;
+    }
+
+
+}
